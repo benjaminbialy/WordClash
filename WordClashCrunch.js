1804ec8c76c305a3ae5e7334d6b96207411d0b2a
Your Name <you@example.com>
2021-09-19 21:50:44 +1000

    Wrote an async function to get API data

--- WordClashCrunch.js
+++ WordClashCrunch.js
@@ -1,1 +1,13 @@
-function callDictionariesAPI(){;}
+// for loop to fetch first word, then second 
+for(let i=1;i<3;i++){
+    // gets the relevant words API response
+    var api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+Word+i.toString();
+    // async function declaration
+    async function getAPIdata(url){
+        // stores the response
+        var response = await fetch(url);
+         // stores data in form of JSON
+        var data = await response.json();
+        }
+    getAPIdata(api_url)
+}
