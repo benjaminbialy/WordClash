5fdbc579fd93554de6df5258a678d2a80b4f3faa
Your Name <you@example.com>
2021-09-21 23:20:14 +1000

    Need to sort the data into it's defintions to then format it.

--- WordClashCrunch.js
+++ WordClashCrunch.js
@@ -1,7 +1,9 @@
+// listens until the button is clicked, then calls the function to call the API
 document.getElementById("WordClashButton").addEventListener("click", callDictionariesAPI);
 function callDictionariesAPI(){
 // for loop to fetch first word, then second 
 for(let i=1;i<3;i++){
+    // gets the input from each field
     var input = document.getElementById("Word"+i).value;
     // gets the relevant words API response
     var api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+input;
@@ -11,8 +13,11 @@ for(let i=1;i<3;i++){
         var response = await fetch(url);
         // stores data in form of JSON
         var data = await response.json();
-        console.log(JSON.stringify(data));
+        // finds the respective paragraph, then pushes the JSON file data in
+        var p = document.getElementById("Definition"+i)
+        p.innerHTML=JSON.stringify(data);
         }
+// makes the API call for the i word
 getAPIdata(api_url);
 }
 ;}
